00438dc4ca9c79d0d8460b19ed703adabe48cad3
eagui020 <eagui020@ucr.edu>
2015-04-28 09:19:34 +0000

    Lab03

---
+++ lab03/lab3.h
@@ -0,0 +1,381 @@
+///
+//  Eric Aguirre
+//  861174273
+//  4/28/15
+//  Make each function and test it one at a time.
+//  Fixed and FLex are interchangable with only difference being the stop point.
+//  For towers think of problems you can do easily then recursively call until
+//  you can do the problem
+///
+#ifndef LAB3_H
+#define LAB3_H
+
+#include <iostream>
+#include <cassert>
+#include <stack>
+
+using namespace std;
+
+template <typename T>
+void showTowerStates(int n, stack<T>& A, stack<T>& B, stack<T>& C)
+{
+    if (n <= 0)
+    {
+        return;
+    }
+    else if (n == 1)
+    {
+        C.push(A.top());
+        A.pop();
+        return;
+    }
+    else if (n == 2)
+    {
+        B.push(A.top());
+        A.pop();
+        C.push(A.top());
+        A.pop();
+        C.push(B.top());
+        B.pop();
+        return;
+    }
+    else if (n == 3) // algorithm for 3 used for n > 3
+    {
+        C.push(A.top());
+        A.pop();
+        B.push(A.top());
+        A.pop();
+        B.push(C.top());
+        C.pop();
+        C.push(A.top());
+        A.pop();
+        A.push(B.top());
+        B.pop();
+        C.push(B.top());
+        B.pop();
+        C.push(A.top());
+        A.pop();
+        return;
+    }
+    else
+    {
+        //look at one less than n if possible to do
+        //Solve for them with aux being destination
+        //Move last in A to final destination
+        //move everything on aux to final destination
+        showTowerStates(n - 1, A, C, B);
+        C.push(A.top());
+        A.pop();
+        --n;
+        showTowerStates(n, B, A, C);
+    }
+    return;
+}
+
+//helper function for tower
+template <typename T>
+void TowerDisplay(stack<T> A, stack<T> B, stack<T> C)
+{
+    if (A.empty()) cout << "Empty" << endl;
+    else
+    {
+        while (!A.empty())
+        {
+            cout << A.top() << " ";
+            A.pop();
+        }
+    }
+    if (B.empty()) cout << "Empty" << endl;
+    else
+    {
+        while (!B.empty())
+        {
+            cout << B.top() << " ";
+            B.pop();
+        }
+    }
+    if (C.empty()) cout << "Empty" << endl;
+    else
+    {
+        while (!C.empty())
+        {
+            cout << C.top() << " ";
+            C.pop();
+        }
+    }
+    cout << endl;
+    return;
+}
+
+template <class T>
+class TwoStackFixed
+{
+    private:
+        T *arr;
+        int sz;
+        int mxtop;
+        int top1; //pos of top of stack1
+        int top2; //pos of top of stack2
+    public:
+        TwoStackFixed(int size, int maxtop)
+        {
+            // constructor that constructs array of ‘size’ length
+            // and constructs empty stacks with maxtop specifying the partition of array for stacks.
+            assert(maxtop < size);
+            sz = size;
+            mxtop = maxtop;
+            arr = new T[size];
+            for (int i = 0; i < size; ++i)
+            {
+                arr[i] = '\0'; //initialize to empty set
+            }
+            top1 = -1;
+            top2 = size;
+        }
+        void pushStack1(T value)
+        {
+            // to push element into first stack. Display the contents of stack
+            // after each push.
+            if (!isFullStack1())
+            {
+                ++top1;
+                arr[top1] = value;
+                display();
+            }
+            else
+            {
+                cout << "Error: End of stack reached" << endl;
+                exit(1);
+            }
+            return;
+        }
+        void pushStack2(T value)
+        {
+            // to push element into second stack. Display the contents of
+            // stack after each push.
+            if (!isFullStack2())
+            {
+                --top2;
+                arr[top2] = value;
+                display();
+            }
+            else
+            {
+                cout << "Error: End of stack reached" << endl;
+                exit(1);
+            }
+            return;
+        }
+        T popStack1()
+        {
+            // to pop element from first stack. Return the popped element and print.
+            if (!isEmptyStack1())
+            {
+                T removed = arr[top1];
+                arr[top1] = '\0'; //set back to null to remove value
+                --top1;
+                return removed;
+            }
+            else
+            {
+                cout << "Error: Stack empty" << endl;
+                exit(1);
+            }
+        }
+        T popStack2()
+        {
+            // to pop element from second stack. Return the popped element and
+            // print.
+            if (!isEmptyStack2())
+            {
+                T removed = arr[top2];
+                arr[top2] = '\0'; //set back to null to remove value
+                ++top2;
+                return removed;
+            }
+            else
+            {
+                cout << "Error: Stack empty" << endl;
+                exit(1);
+            }
+        }
+        bool isFullStack1()
+        {
+            // helper function to check whether first stack is full.
+            return (top1 == mxtop - 1) ? true : false; //stack 2 takes mxtop
+        }
+        bool isFullStack2()
+        {
+            // helper function to check whether second stack is full.
+            return (top2 == mxtop) ? true : false;
+        }
+        bool isEmptyStack1()
+        {
+            // helper function to check whether first stack is empty.
+            return (top1 == -1) ? true : false;
+        }
+        bool isEmptyStack2()
+        {
+            // helper function to check whether second stack is empty.
+            return (top2 == sz) ? true : false;
+        }
+        void display()
+        {
+            if (sz == 0)
+            {
+                cout << "Nothing to display." << endl;
+                return;
+            }
+            // to display contents of array.
+            for (int i = 0; i < sz; ++i)
+            {
+                if (arr[i] != '\0') // empty set is a space
+                {
+                    cout << arr[i] << " ";
+                }
+                else
+                {
+                    cout << " ";
+                }
+            }
+            cout << endl;
+            return;
+        }
+};
+
+// Only going to be difference in check if full
+template <class T>
+class TwoStackOptimal
+{
+    private:
+        T *arr;
+        int sz;
+        int top1;
+        int top2;
+    public:
+        TwoStackOptimal(int size)
+        {
+            // constructor that constructs array of size and constructs empty
+            // stacks.
+            sz = size;
+            arr = new T[size];
+            for (int i = 0; i < size; ++i)
+            {
+                arr[i] = '\0';
+            }
+            top1 = -1;
+            top2 = size;
+        }
+        void pushFlexStack1(T value)
+        {
+            // to push element into first stack. Display the contents of stack
+            // after each push.
+            if (!isFullStack1())
+            {
+                ++top1;
+                arr[top1] = value;
+                display();
+            }
+            else
+            {
+                cout << "Error: End of stack reached" << endl;
+                exit(1);
+            }
+            return;
+        }
+        void pushFlexStack2(T value)
+        {
+            // to push element into second stack. Display the contents of
+            // stack after each push.
+            if (!isFullStack2())
+            {
+                --top2;
+                arr[top2] = value;
+                display();
+            }
+            else
+            {
+                cout << "Error: End of stack reached" << endl;
+                exit(1);
+            }
+            return;
+        }
+        T popFlexStack1()
+        {
+            // to pop element into first stack. Return the popped element and print.
+            if (!isEmptyStack1())
+            {
+                T removed = arr[top1];
+                arr[top1] = '\0';
+                --top1;
+                return removed;
+            }
+            else
+            {
+                cout << "Error: Stack empty" << endl;
+                exit(1);
+            }
+        }
+        T popFlexStack2()
+        {
+            // to pop element from second stack. Return the popped element and
+            // print.
+            if (!isEmptyStack2())
+            {
+                T removed = arr[top2];
+                arr[top2] = '\0';
+                ++top2;
+                return removed;
+            }
+            else
+            {
+                cout << "Error: Stack empty" << endl;
+                exit(1);
+            }
+        }
+        bool isFullStack1()
+        {
+            // helper function to check whether first stack is full.
+            return (top1 == top2 - 1) ? true : false; //only difference
+        }
+        bool isFullStack2()
+        {
+            // helper function to check whether second stack is full.
+            return (top2 == top1 + 1) ? true : false; //only difference
+        }
+        bool isEmptyStack1()
+        {
+            // helper function to check whether first stack is empty.
+            return (top1 == -1) ? true : false;
+        }
+        bool isEmptyStack2()
+        {
+            // helper function to check whether second stack is empty.
+            return (top2 == sz) ? true : false;
+        }
+        void display()
+        {
+            // to display contents of array.
+            if (sz == 0)
+            {
+                cout << "Nothing to display." << endl;
+                return;
+            }
+            // to display contents of array.
+            for (int i = 0; i < sz; ++i)
+            {
+                if (arr[i] != '\0')
+                {
+                    cout << arr[i] << " ";
+                }
+                else
+                {
+                    cout << " ";
+                }
+            }
+            cout << endl;
+            return;
+        }
+        
+};
+#endif
---
+++ lab03/main.cpp
@@ -0,0 +1,84 @@
+///
+//  Eric Aguirre
+//  861174273
+//  4/28/15
+//  Make each function and test it one at a time.
+//  Fixed and FLex are interchangable with only difference being the stop point.
+//  For towers think of problems you can do easily then recursively call until
+//  you can do the problem
+///
+#include "lab3.h"
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main() {
+    // Ex 1
+    TwoStackFixed<int> test1(5, 3);
+    test1.pushStack1(5);
+    test1.pushStack2(4);
+    test1.pushStack2(3);
+    // test1.pushStack2(4);
+    test1.pushStack1(4);
+    test1.pushStack1(7);
+    
+    cout << test1.popStack1() << endl;
+    test1.display();
+    cout << test1.popStack1() << endl;
+    test1.display();
+    
+    //char
+    TwoStackFixed<char> test2(5, 3);
+    test2.pushStack1('a');
+    test2.pushStack2('d');
+    test2.pushStack2('e');
+    test2.pushStack1('h');
+    test2.pushStack1('f');
+    
+    cout << test2.popStack1() << endl;
+    test2.display();
+    cout << test2.popStack1() << endl;
+    test2.display();
+    cout << test2.popStack1() << endl;
+    test2.display();
+    // cout << test2.popStack1() << endl; //empty test
+    // test2.display();
+    cout << endl;
+    
+    //Ex2
+    cout << "Ex2" << endl;
+    TwoStackOptimal<int> test3(5);
+    test3.pushFlexStack1(5);
+    test3.pushFlexStack1(4);
+    test3.pushFlexStack2(3);
+    test3.pushFlexStack2(4);
+    test3.pushFlexStack1(7);
+    // test3.pushFlexStack1(7);
+    
+    cout << test3.popFlexStack1() << endl;
+    test3.display();
+    cout << test3.popFlexStack1() << endl;
+    test3.display();
+    cout << test3.popFlexStack1() << endl;
+    test3.display();
+    // cout << test3.popFlexStack1() << endl; //empty test
+    // test3.display();
+    cout << endl;
+    
+    //Ex3
+    cout << "Ex3" << endl;
+    stack<int> start;
+    stack<int> aux;
+    stack<int> dest;
+    int i;
+    for (i = 1; i <= 20; ++i) // Change i <= value for different cases
+    {
+        start.push(i);
+    }
+    // cout << i << endl;
+    showTowerStates(i-1, start, aux, dest);
+    TowerDisplay(start, aux, dest);
+    
+    return 0;    
+}
